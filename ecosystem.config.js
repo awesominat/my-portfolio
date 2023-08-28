c5a174bd524fda0aaeb729114fc7988e37100174
awesominat <blackmg51055@gmail.com>
2023-08-28 19:40:26 +0400

    showed build

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -14,8 +14,8 @@ module.exports = {
       repo : 'git@github.com:awesominat/my-portfolio.git',
       path : '/home/ubuntu/portfolio',
       'pre-deploy-local': '',
-      'post-deploy' : 'rm -rf controllers models routes && mv backend/controllers backend/hash_pass.js backend/index.js backend/models backend/package-lock.json backend/package.json backend/routes backend/skills_to_json.js ./ && npm install && rm -rf backend/ && find frontend/* -maxdepth 0 ! -name build -exec rm -rf {} + && pm2 reload ecosystem.config.js --env production',
-      // 'post-deploy' : 'rm -rf controllers models routes && mv backend/controllers backend/hash_pass.js backend/index.js backend/models backend/package-lock.json backend/package.json backend/routes backend/skills_to_json.js ./ && npm install && rm -rf backend/ && pm2 reload ecosystem.config.js --env production',
+      // 'post-deploy' : 'rm -rf controllers models routes && mv backend/controllers backend/hash_pass.js backend/index.js backend/models backend/package-lock.json backend/package.json backend/routes backend/skills_to_json.js ./ && npm install && rm -rf backend/ && find frontend/* -maxdepth 0 ! -name build -exec rm -rf {} + && pm2 reload ecosystem.config.js --env production',
+      'post-deploy' : 'rm -rf controllers models routes && mv backend/controllers backend/hash_pass.js backend/index.js backend/models backend/package-lock.json backend/package.json backend/routes backend/skills_to_json.js ./ && npm install && rm -rf backend/ && pm2 reload ecosystem.config.js --env production',
       'pre-setup': ''
     }
   }
